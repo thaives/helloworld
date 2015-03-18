f3aafdaf3672802b94823e1706db24a0b14622c2
Thai Pham <phamhuynhhthai@gmail.com>
2015-03-17 22:02:12 -0400

    add helloworl main class

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello hello = new Hello("English");
+		GoodBye bye = new GoodBye("English");
+		hello.printMsgOut();
+		bye.printMsgOut();
+	}
+
+}
